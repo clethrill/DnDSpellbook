4c4f2a99e29c0b8dd9c1032764f7c6e8723e4b0c
Brett <brettalexwilson@gmail.com>
2016-11-26 17:35:53 +1100

    Filtering for names of spells

--- src/entry.js
+++ src/entry.js
@@ -43,17 +43,15 @@ class Main extends Component {
 		super (props);
 		this.state = {
 			spells: [
-				"Fireball",
-				"Firebolt"
-			]
+				{name: "acid splash", level: 0, school: "abjuration"},
+				{name: "fireball", level: 4, school: "conjuration"},
+				{name: "firebolt", level: 0, school: "conjuration"}
+			],
+			input: ""
 		};
-
+		/*
 		function GETcall(url, callbacks) {
 			$.ajax({
-				// the url something like
-				/*
-					/api/echo
-				*/
 				url: url,
 
 				//this is a get call
@@ -67,19 +65,66 @@ class Main extends Component {
 		GETcall("https://te3fmtf49g.execute-api.ap-southeast-2.amazonaws.com/dev/api/hello", {
 			success: (data) => {console.log(data)},
 			error: (error) => {console.log(error)}
-		})
+		}) */
+
+		this.handleChange = this.handleChange.bind(this);
+		this.spellList = this.spellList.bind(this);
+		this.loadList = this.loadList.bind(this);
 	}
+
+	handleChange(e) {
+		this.setState({input: e.target.value});
+	}
+
+	spellList(level) {
+		let cantripList = this.state.spells.map((value, index) => {
+			if (value.name.indexOf(this.state.input) >= 0 && value.level == level)
+			return (<Spell value={value.name} key={index} />);
+		});
+
+		return cantripList;
+	}
+
+	loadList(name, list, section) {
+		let newList = new Array();
+		for(var i = 0; i < list.length; i++) {
+			if(list[i]) {
+				newList.push(list[i]);
+			}
+		}
+
+		console.log(name, newList)
+		var section;
+		if (newList.length > 0) {
+			section = (
+				<div>
+					<h2>{name}</h2>
+					<ul>
+						{newList}
+					</ul>
+				</div>
+			)
+		}
+		return section;
+	}
+
+	printSpells(spellList) {
+		return spellList.map((value, index) =>{
+			return value;
+		});
+	}
+
 	render() {
-		let spellList = this.state.spells.map((value, index) => {
-			return (<Spell value={value} key={index} />);
-		})
+		let spells = [];
+		spells[0] = this.loadList("Cantrips", this.spellList(0));
+		spells[4] = this.loadList("Level 4", this.spellList(4));
+
 		return (
 			<div>
 				<Nav />
 				<h1>Spells {this.props.name}</h1>
-				<ul>
-					{spellList}
-				</ul>
+				<input values={this.state.input} onChange={this.handleChange} />
+				{this.printSpells(spells)}
 	 	 	</div>
 
 		);
