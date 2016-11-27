eac12ee247f3c62ac6202414439f8c31a739d11b
Brett <brettalexwilson@gmail.com>
2016-11-27 11:39:26 +1100

    Sandon go

--- src/spelldetail.js
+++ src/spelldetail.js
@@ -1,12 +1,78 @@
 import React, {Component} from 'react';
 
 export default class SpellDetail extends Component {
+	constructor (props) {
+		super (props);
+
+		this.components = this.components.bind(this);
+
+		console.log("kjgkhjgk")
+	}
+
+	components() {
+		//return this.props.data[value] ? output : "";
+		var output = "Hey";
+		console.log("Heya");
+		if(this.props.data.ComponentsVocal)
+			output.concat("V ");
+		if(this.props.data.ComponentsSomatic)
+			output.concat("S ");
+		if(this.props.data.ComponentsMaterial)
+		{
+			output.concat("M ");
+			output.concat(this.props.data.ComponentsMaterialExtra);
+		}
+		console.log(output, "Heya");
+
+		return output;
+	}
+
 	render() {
 		return (
 			<section>
 				<div className="row">
 					<div className="col-xs-12">
 						<h2>{this.props.data.Name}</h2>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-xs-12">
+						<p> Spell Level: {this.props.data.Level == 0 ? "Cantrip" : this.props.data.Level}</p>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-xs-12">
+						<p>School: {this.props.data.School}</p>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-xs-12">
+						<p>Range: {this.props.data.Range}</p>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-xs-12">
+						<p>Casting Time: {this.props.data.CastingTime}</p>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-xs-12">
+						<p>Components: {this.components}</p>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-xs-12">
+						<p>Duration: {this.props.data.Duration}</p>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-xs-12">
+						<p>School: {this.props.data.School}</p>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-xs-12">
+						<p>Duration: {this.props.data.Duration}</p>
 					</div>
 				</div>
 				<div className="row">
--- src/spellview.js
+++ src/spellview.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {data} from './data.js';
 import nav from './nav.js';
+import SpellDetail from './spelldetail.js';
 
 //{name: "Elmon's Spellbook", id: 1, spells: [1,2,3]},
 //{name: "Bard's Book", id: 2, spells: [1,3,5]}
@@ -32,11 +33,31 @@ class spellview extends Component {
 	}
 
 	render () {
+		let content = this.state.SpellData.map((value, index) => {
+			return (
+
+			)
+			 (<SpellDetail data={this.state.spellData[this.props.context-1]} />);
+		});
+		let backButton = (<span onClick={this.goToList}>Back</span>);
+
 		return (
-			<nav />
-			<ul>
-				{this.loadSpellbook}
-			</ul>
+			<main>
+				<Nav />
+				<div className="row">
+					<div className="col-xs-8">
+						<h1>Spells</h1>
+					</div>
+					<div className="col-xs-4">
+						{backButton}
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-xs-12">
+						{content}
+					</div>
+				</div>
+	 	 	</main>
 		)
 	}
 
